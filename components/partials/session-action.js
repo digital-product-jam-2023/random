b56d58e9c124cfd48c277457537a3b98a9fee5a2
Noya Antman Ron <noya@niio.com>
2022-11-18 17:43:31 +0200

    first commit

--- components/partials/session-action.js
+++ components/partials/session-action.js
@@ -1,12 +1,10 @@
 export default function SessionAction({ id, text, handler, disabled }) {
   const buttonText = disabled ? "Wait ..." : text;
   return (
-    <div className="row">
-      <div className="item">
-        <div className="content">
-          <button id={id} onClick={handler} disabled={disabled}>{buttonText}</button>
-        </div>
-      </div>
+    <div className="action" id={id}>
+      <button id={id} onClick={handler} disabled={disabled}>
+        {buttonText}
+      </button>
     </div>
-  )
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,18 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+Hebrew:wght@900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
