945daa8791b8ef8fc5e5ec0c2edf46bd6329b333
Noya Antman Ron <noya@niio.com>
2022-11-21 18:23:21 +0200

    added background color animation

--- components/partials/student-list.js
+++ components/partials/student-list.js
@@ -3,33 +3,46 @@ export default function StudentList({
   students,
   currentTeamMembers,
   assignedStudents,
-  animate
+  animate,
 }) {
   const currentTeamSelected = currentTeamMembers.length > 0;
-  const rowClassName = animate? "row animate": "row";
+  const rowClassName = animate ? "row animate" : "row";
+  const body = document.querySelector("body");
+  if (animate) {
+    body.classList.add("animated-body");
+  } else {
+    body.classList.remove("animated-body");
+  }
   return (
     <div className="row">
       {groups.map((group) => {
         return (
           <div key={group.id} className="item students">
-            <h1 className="content group-title">
-              {group.name}
-            </h1>
+            <h1 className="content group-title">{group.name}</h1>
             <div className={rowClassName}>
-              {students.filter((student) => student.group.id === group.id).map((student) => {
-                const studentInCurrentTeam = currentTeamMembers.includes(student.id);
-                const studentHide = currentTeamSelected && !studentInCurrentTeam;
-                const studentHasBeenAssigned = assignedStudents.includes(student.id);
-                let className = "item";
-                if (studentHide) className = `${className} hide`;
-                if (studentInCurrentTeam) className = `${className} current-team-member`;
-                if (studentHasBeenAssigned) className = `${className} assigned-student`;
-                return (
-                  <div key={student.id} className={className}>
-                    <div className="content">{student.name}</div>
-                  </div>
-                );
-              })}
+              {students
+                .filter((student) => student.group.id === group.id)
+                .map((student) => {
+                  const studentInCurrentTeam = currentTeamMembers.includes(
+                    student.id
+                  );
+                  const studentHide =
+                    currentTeamSelected && !studentInCurrentTeam;
+                  const studentHasBeenAssigned = assignedStudents.includes(
+                    student.id
+                  );
+                  let className = "item";
+                  if (studentHide) className = `${className} hide`;
+                  if (studentInCurrentTeam)
+                    className = `${className} current-team-member`;
+                  if (studentHasBeenAssigned)
+                    className = `${className} assigned-student`;
+                  return (
+                    <div key={student.id} className={className}>
+                      <div className="content">{student.name}</div>
+                    </div>
+                  );
+                })}
             </div>
           </div>
         );
